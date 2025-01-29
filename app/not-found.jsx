abdc8c4aa310af734b195a437bfb2484af85afc5
MukitHossen7 <hossenmukit7@gmail.com>
2025-01-29 16:32:22 +0600

    add error handling for blog details; create Not Found page for missing resources

---
+++ app/not-found.jsx
@@ -0,0 +1,12 @@
+const NotFoundPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center py-32">
+      <h2 className="text-gray-200 font-semibold text-2xl">Not Found</h2>
+      <p className="font-medium text-gray-400">
+        Could not find requested resource
+      </p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
