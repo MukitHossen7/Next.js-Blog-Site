e1b6fac6679ae1029f3d7321d846a13e736056a6
MukitHossen7 <hossenmukit7@gmail.com>
2025-01-29 16:18:04 +0600

    add loading component with spinner; update package dependencies

---
+++ app/loading.jsx
@@ -0,0 +1,11 @@
+import { RiseLoader } from "react-spinners";
+
+const IsLoading = () => {
+  return (
+    <div className="flex flex-col items-center justify-center py-24">
+      <RiseLoader color="#0eb6b6" />
+    </div>
+  );
+};
+
+export default IsLoading;
