b83c0c4c5dfca38220c75e79bc3cca32ca6c1a70
Rokib Shuvo <itismehey975@gmail.com>
2025-03-21 11:33:20 +0600

    added from rokib shuvo

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,6 +4,7 @@ const Footer = () => {
       <p className="font-semibold text-center text-sm text-gray-300 py-4">
         �� 2023 Next.js Blog. All rights reserved.
       </p>
+      <h2>this line written by rokib </h2>
     </div>
   );
 };
