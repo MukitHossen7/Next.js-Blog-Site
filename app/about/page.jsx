0f1543209be1ea8fa6e7d51703c939ccbd88e347
Rokib Shuvo <itismehey975@gmail.com>
2025-03-21 11:15:41 +0600

    added from rokib dev

---
+++ app/about/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <>added from rokib to check merging</>
+        </div>
+    );
+};
+
+export default page;
